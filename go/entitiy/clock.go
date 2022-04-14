a17bbd00e4534bcf8c3d2cabd9c645455de127bb
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2022-04-14 11:10:32 +0900

    add new func FetchClockToAngle

--- go/entitiy/clock.go
+++ go/entitiy/clock.go
@@ -1,10 +1,14 @@
 package entitiy
 
-func fetchShortHandAngle(currentHour int, currentMinute int) float64 {
+func FetchShortHandAngle(currentHour int, currentMinute int) float64 {
 	return 0.1
 }
 
-func fetchLongtHandAngle(currentMinute int) float64 {
+func FetchLongtHandAngle(currentMinute int) float64 {
 	angleByOneMin := 6.0
 	return float64(currentMinute) * angleByOneMin
+}
+
+func FetchClockToAngle(currentHour int, currentMinute int) float64 {
+	return 0.0
 }
