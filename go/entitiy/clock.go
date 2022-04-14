5f31926d975f365eade2307e8394e13decbd5f83
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2022-04-14 11:25:49 +0900

    create algo

--- go/entitiy/clock.go
+++ go/entitiy/clock.go
@@ -1,14 +1,21 @@
 package entitiy
 
-func FetchShortHandAngle(currentHour int, currentMinute int) float64 {
-	return 0.1
+func fetchShortHandAngle(currentHour int, currentMinute int) float64 {
+	angleByOneHour := 30.0
+	angleByOneMin := 0.5
+	return float64(currentHour)*angleByOneHour + float64(currentMinute)*angleByOneMin
 }
 
-func FetchLongtHandAngle(currentMinute int) float64 {
+func fetchLongtHandAngle(currentMinute int) float64 {
 	angleByOneMin := 6.0
 	return float64(currentMinute) * angleByOneMin
 }
 
 func FetchClockToAngle(currentHour int, currentMinute int) float64 {
-	return 0.0
+	shortHandAngle := fetchShortHandAngle(currentHour, currentMinute)
+	longHandAngle := fetchLongtHandAngle(currentMinute)
+	if shortHandAngle > longHandAngle {
+		return shortHandAngle - longHandAngle
+	}
+	return longHandAngle - shortHandAngle
 }
--- go/main.go
+++ go/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"time"
+
+	"github.com/yuta519/clock_to_angle/go/entitiy"
+)
 
 func main() {
-	fmt.Println("hoge")
+	currentTime := time.Now()
+
+	fmt.Printf(
+		"A current time is `%s:%s`\n",
+		strconv.Itoa(currentTime.Hour()),
+		strconv.Itoa(currentTime.Minute()),
+	)
+
+	fmt.Printf(
+		"An angle between hands is %s°\n",
+		strconv.FormatFloat(entitiy.FetchClockToAngle(currentTime.Hour(), currentTime.Minute()), 'f', 1, 64),
+	)
+
 }
